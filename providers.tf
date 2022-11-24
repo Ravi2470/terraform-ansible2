83701aee3ee7eb53d2a07756afd4b319a3174983
Ravi2470 <109647098+Ravi2470@users.noreply.github.com>
2022-11-24 22:54:05 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "aws" {
   region     = "ap-northeast-1"
-  access_key = "AKIA4PQ5PXF3E5H5UVXQ"
-  secret_key = "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"
+  access_key = "access-key"
+  secret_key = "secret-key"
 }
 
